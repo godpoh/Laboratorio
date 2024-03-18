f33a71c3e19257076996d21cffe9415fcba57fb9
godpoh <jeremy12troll@gmail.com>
2024-03-18 09:10:10 -0600

    xd

---
+++ main.py
@@ -0,0 +1,28 @@
+print("1. Visualizar estadio y precios de los asientos.")
+print("2. Registro de espectadores.")
+print("3. Compra de entradas.")
+print("4. Reporte de Ventas")
+print("5. Salir.")
+
+while True:
+    options = input("Escribe una de las opciones: ")
+
+    if options == "1":
+        print("Ha ingresado la opcion 1. ")
+    elif options == "2":
+        print("Ha ingresado la opcion 2. ")
+    elif options == "3":
+        print("Ha ingresado la opcion 3.")
+    elif options == "4":
+        print("Ha ingresado la opcion 4")
+    elif options == "5":
+        print("Ha ingresado la opcion 5")
+        break
+    else:
+        print("Opcion invalida")
+
+
+
+
+
+21316456
